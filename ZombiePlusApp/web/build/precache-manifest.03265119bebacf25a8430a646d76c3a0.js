32acd7ade1065b7cac2148b022eb221058ed9b17
Hugo <victorfonsecasp@gmail.com>
2024-06-26 22:08:27 +0100

    Implementation of user interface automation scripts using playwright
    Adding ZombiePlus source code as well

---
+++ ZombiePlusApp/web/build/precache-manifest.03265119bebacf25a8430a646d76c3a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266444f82662c9f47dd9472cf90ab6f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bff821ebecac634ee8",
+    "url": "/static/css/2.90779e30.chunk.css"
+  },
+  {
+    "revision": "a7bff821ebecac634ee8",
+    "url": "/static/js/2.ce60308f.chunk.js"
+  },
+  {
+    "revision": "93041e6313bb308a056e501f1fbe249b",
+    "url": "/static/js/2.ce60308f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514dd5f100ad4794a5f5",
+    "url": "/static/js/main.30c206b5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.30c206b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb95257f7af07971b32",
+    "url": "/static/js/runtime-main.8b40e423.js"
+  },
+  {
+    "revision": "8aeaaf22745541fadcfb54504be112fe",
+    "url": "/static/media/callie.8aeaaf22.svg"
+  },
+  {
+    "revision": "9fccd4f2a7e6f9cfa72ffacfb55ad1c9",
+    "url": "/static/media/logo.9fccd4f2.svg"
+  },
+  {
+    "revision": "2456d47b21c7e7664807cdf86c357948",
+    "url": "/static/media/zombie.2456d47b.svg"
+  }
+]);
---
+++ ZombiePlusApp/web/build/precache-manifest.66a49fbfb34ecb1ad14ea2ef16e1b1a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e25a73857d6935d1ac0f5770b0965af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bff821ebecac634ee8",
+    "url": "/static/css/2.90779e30.chunk.css"
+  },
+  {
+    "revision": "a7bff821ebecac634ee8",
+    "url": "/static/js/2.ce60308f.chunk.js"
+  },
+  {
+    "revision": "93041e6313bb308a056e501f1fbe249b",
+    "url": "/static/js/2.ce60308f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257b74a280ef2a68b88d",
+    "url": "/static/js/main.e51f5af3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e51f5af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb95257f7af07971b32",
+    "url": "/static/js/runtime-main.8b40e423.js"
+  },
+  {
+    "revision": "8aeaaf22745541fadcfb54504be112fe",
+    "url": "/static/media/callie.8aeaaf22.svg"
+  },
+  {
+    "revision": "9fccd4f2a7e6f9cfa72ffacfb55ad1c9",
+    "url": "/static/media/logo.9fccd4f2.svg"
+  },
+  {
+    "revision": "2456d47b21c7e7664807cdf86c357948",
+    "url": "/static/media/zombie.2456d47b.svg"
+  }
+]);
